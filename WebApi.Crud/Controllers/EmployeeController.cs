485bc983c5a1d96a2f707cf5ca1704e7f4991487
“Moriarty” <Akankshaverma005@gmail.com>
2022-05-22 16:32:34 +0530

    * .gitignore: Initial push
    * WebApi.Crud.sln:
    * Program.cs:
    * Startup.cs:
    * appsettings.json:
    * WeatherForecast.cs:
    * WebApi.Crud.csproj:
    * appsettings.Development.json:
    * launchSettings.json:
    * EmployeeController.cs:
    * WeatherForecastController.cs:

---
+++ WebApi.Crud/Controllers/EmployeeController.cs
@@ -0,0 +1,73 @@
+﻿using System;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using WebApi.Crud.Data;
+using WebApi.Crud.Models;
+
+namespace WebApi.Crud.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class EmployeeController : ControllerBase
+    {
+        public readonly ApplicationDbContext _db;
+        public EmployeeController(ApplicationDbContext db)
+        {
+            _db = db;
+        }
+
+        //Defining Action methods for the API
+
+        [HttpGet]
+        public IActionResult GetEmployees()
+        {
+            return Ok(_db.Employees.ToList());
+        }
+
+        [HttpPost]
+        public async Task<IActionResult> AddEmployee([FromBody] Employee objEmployee)
+        {
+            if (!ModelState.IsValid)
+            {
+                return new JsonResult("Error while adding a new employee");
+            }
+            _db.Employees.Add(objEmployee);
+            await _db.SaveChangesAsync();
+
+            return new JsonResult("Employee added successfully");
+
+        }
+
+        [HttpPut("{id}")]
+        public async Task<IActionResult> UpdateEmployee([FromRoute] int id, [FromBody] Employee objEmployee)
+        {
+            if(objEmployee == null || id != objEmployee.id)
+            {
+                return new JsonResult("Employee does not exist!");
+            }
+            else
+            {
+                _db.Employees.Update(objEmployee);
+                await _db.SaveChangesAsync();
+
+                return new JsonResult("Employee data updated successfully");
+            }
+        }
+        [HttpDelete("{id}")]
+        public async Task<IActionResult> DeleteEmployee([FromRoute] int id)
+        {
+            var findEmp = await _db.Employees.FindAsync(id);
+            if (findEmp == null)
+            {
+                return NotFound();
+            }
+            else
+            {
+                _db.Employees.Remove(findEmp);
+                await _db.SaveChangesAsync();
+                return new JsonResult("Employee Deleted successfully");
+            }
+        }        
+    }
+}
---
+++ WebApi.Crud/Startup.cs
@@ -0,0 +1,55 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Builder;
+using Microsoft.AspNetCore.Hosting;
+using Microsoft.AspNetCore.HttpsPolicy;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.DependencyInjection;
+using Microsoft.Extensions.Hosting;
+using Microsoft.Extensions.Logging;
+using WebApi.Crud.Data;
+
+namespace WebApi.Crud
+{
+    public class Startup
+    {
+        public Startup(IConfiguration configuration)
+        {
+          config = configuration;
+        }
+
+        public IConfiguration config { get; set; }
+
+        // This method gets called by the runtime. Use this method to add services to the container.
+        public void ConfigureServices(IServiceCollection services)
+        {
+
+            services.AddControllers();  
+            services.AddDbContext<ApplicationDbContext>(options => options.UseSqlServer(config.GetConnectionString("SqlServer")));
+        }
+
+        // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
+        public void Configure(IApplicationBuilder app, IWebHostEnvironment env)
+        {
+            if (env.IsDevelopment())
+            {
+                app.UseDeveloperExceptionPage();
+            }
+
+            app.UseHttpsRedirection();
+
+            app.UseRouting();
+
+            app.UseAuthorization();
+
+            app.UseEndpoints(endpoints =>
+            {
+                endpoints.MapControllers();
+            });
+        }
+    }
+}
